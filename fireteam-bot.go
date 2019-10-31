f1be49e9fed5e56d113cf06c09d1a25877da94ec
Thomas Lagrange <tplagrange@gmail.com>
2019-10-31 15:02:56 -0400

    Initial Gin Server

---
+++ fireteam-bot.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+
+    "github.com/gin-gonic/gin"
+)
+
+func hello(c *gin.Context) {
+    c.String(http.StatusOK, "Hello, world!")
+}
+
+func main() {
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        fmt.Print("Using default port...")
+        port = "8080"
+    }
+
+    // Create router
+    router := gin.New()
+
+    // Define plugins
+    router.Use(gin.Logger())
+
+    // Define routes
+    router.GET("/", hello)
+
+    router.Run(":" + port)
+}
