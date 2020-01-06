4e727fe4aa29a18e343f5c526c53c8164e7f6cd8
Thomas Lagrange <tplagrange@gmail.com>
2020-01-06 15:24:19 -0500

    Tweak callback url

--- routes.go
+++ routes.go
@@ -7,7 +7,7 @@ import (
 func initRoutes(router *gin.Engine) {
     router.GET("/", hello)
 
-    router.GET("/api/bungie/callback/:code", bungieCallback)
+    router.GET("/api/bungie/callback", bungieCallback)
     router.GET("/api/bungie/auth/:payload", bungieAuth)
 
     router.GET("/api/loadout/:id", getLoadout)
